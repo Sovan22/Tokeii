c1452ed0ba8f401e7b4bd36fd072547128610098
Sovan22 <deepsin059@gmail.com>
2023-09-05 14:26:18 +0530

    added toolbar and popular trending tv shows and movies

--- app/src/main/java/com/demomiru/tokeiv2/MainActivity.kt
+++ app/src/main/java/com/demomiru/tokeiv2/MainActivity.kt
@@ -1,46 +1,44 @@
 package com.demomiru.tokeiv2
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
+
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.gargoylesoftware.htmlunit.javascript.host.WindowOrWorkerGlobalScopeMixin.atob
-import it.skrape.core.htmlDocument
-import it.skrape.fetcher.BrowserFetcher
-import it.skrape.fetcher.HttpFetcher
-import it.skrape.fetcher.extract
-import it.skrape.fetcher.response
-import it.skrape.fetcher.skrape
-import it.skrape.selects.attribute
+import kotlinx.coroutines.DelicateCoroutinesApi
+
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.apache.commons.io.FileUtils.waitFor
-import kotlinx.coroutines.*
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-import java.util.Base64
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvRc: RecyclerView
+    private lateinit var tvCardRc: RecyclerView
     private lateinit var movieRc: RecyclerView
-
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Toolbar
+        val myToolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(myToolbar)
+        supportActionBar?.title = "Tokei"
+
+
+        tvCardRc = findViewById(R.id.card_container)
         tvRc = findViewById(R.id.tv_recycler_view)
         movieRc = findViewById(R.id.movie_recycler_view)
+
+        tvCardRc.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         tvRc.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         movieRc.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
 
@@ -53,20 +51,39 @@ class MainActivity : AppCompatActivity() {
         val movieService = retrofit.create(MovieService::class.java)
 
         GlobalScope.launch(Dispatchers.Main) {
-            val tvResponse = tvService.getPopularTVShows(
+            val tvPopularResponse = tvService.getPopularTVShows(
                 "cab731891b28c5ad61c85cd993851ed7",
                 "en-US"
             )
-            val movieResponse = movieService.getPopularMovies("cab731891b28c5ad61c85cd993851ed7",
-                "en-US")
-            if (tvResponse.isSuccessful) {
-                val tvShows = tvResponse.body()?.results ?: emptyList()
-                tvRc.adapter = TVShowAdapter(tvShows)
+
+            val tvTrendingResponse = tvService.getTrendingTVShows(
+                "cab731891b28c5ad61c85cd993851ed7",
+                "en-US"
+            )
+
+            val movieResponse = movieService.getPopularMovies(
+                "cab731891b28c5ad61c85cd993851ed7",
+                "en-US"
+            )
+
+
+            if (tvTrendingResponse.isSuccessful) {
+                val tvShows = tvTrendingResponse.body()?.results ?: emptyList()
+                Log.i("Trending Shows Size", tvShows.size.toString())
+                val trendingTVShows = tvShows.subList(0,18).chunked(3)
+                tvCardRc.adapter = TVShowCardAdapter(trendingTVShows)
             }
+
             if (movieResponse.isSuccessful) {
                 val movies = movieResponse.body()?.results ?: emptyList()
                 movieRc.adapter = MovieAdapter(movies)
             }
+
+            if (tvPopularResponse.isSuccessful) {
+                val tvShows = tvPopularResponse.body()?.results ?: emptyList()
+                tvRc.adapter = TVShowAdapter(tvShows)
+            }
+
         }
     }
 }
