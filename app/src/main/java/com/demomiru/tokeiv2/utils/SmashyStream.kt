9967bf720e733b09b9520136f1250f82ac582adb
Sovan22 <deepsin059@gmail.com>
2023-11-11 01:49:04 +0530

    fix SmashyStream - get data from json instead html doc

--- app/src/main/java/com/demomiru/tokeiv2/utils/SmashyStream.kt
+++ app/src/main/java/com/demomiru/tokeiv2/utils/SmashyStream.kt
@@ -1,15 +1,19 @@
 package com.demomiru.tokeiv2.utils
 
-import com.demomiru.tokeiv2.BuildConfig
+
 import com.google.gson.Gson
 import com.lagradost.nicehttp.Requests
 
 
+data class SmashyData(
+    val sourceUrls: ArrayList<String> = arrayListOf(),
+    val subtitleUrls: String? = null
+)
 class SmashyStream{
 
     private val app = Requests()
     private val gson = Gson()
-    private val proxy = BuildConfig.PROXY_URL
+//    private val proxy = BuildConfig.PROXY_URL
     suspend fun getLink(isMovie: Boolean = false,id: String,s: Int,e:Int) : Pair<String?,String?>{
         val getUrl = if(!isMovie) "https://embed.smashystream.com/playere.php?tmdb=$id&season=$s&episode=$e" else "https://embed.smashystream.com/playere.php?tmdb=$id"
         try {
@@ -19,34 +23,40 @@ class SmashyStream{
                     referer = "https://smashystream.xyz/"
                 ).document
                     .select("div.dropdown-menu a.server.dropdown-item").map {
-                        it.attr("data-id")
+                        it.attr("data-url")
                     }
             println(sources)
 
             val streamRes = app.get(
                 sources[0],
                 referer = getUrl
-            ).document.getElementsByTag("script")
+            ).toString()
+//                .document.getElementsByTag("script")
+            val streamParsed = gson.fromJson(streamRes,SmashyData::class.java)
+//            return Pair(streamParsed.sourceUrls[0],streamParsed.subtitleUrls[0])
 
-            val content = streamRes[1].html()
-
-            val filePattern = """"file":\s*"([^"]*)"""".toRegex()
-            val subtitlePattern = """"subtitle":\s*"([^"]*)"""".toRegex()
-            val fileMatchResult = filePattern.find(content)
-            val subMatchResult = subtitlePattern.find(content)
-            val fileValue = fileMatchResult?.groups?.get(1)?.value
-            val subtitleValue = subMatchResult?.groups?.get(1)?.value
+//            val content = streamRes
+////            [1].html()
+//
+//            val filePattern = """"file":\s*"([^"]*)"""".toRegex()
+//            val subtitlePattern = """"subtitle":\s*"([^"]*)"""".toRegex()
+//            val fileMatchResult = filePattern.find(content)
+//            val subMatchResult = subtitlePattern.find(content)
+//
+//
+//            val fileValue = fileMatchResult?.groups?.get(1)?.value
+//            val subtitleValue = subMatchResult?.groups?.get(1)?.value
 
 //            val subSource =
 //                subtitleValue?.substring(subtitleValue.indexOf("]") + 1)?.replace("\\", "")
 //                    ?.replace(",", "")
 //            println("Sub: $subSource")
             var subSource: String? = null
-            if(subMatchResult!=null){
-                val subSources = subtitleValue?.split(",")?.toMutableList()
-                subSources?.remove("")
+            if(streamParsed.subtitleUrls!=null){
+                val subSources = streamParsed.subtitleUrls.split(",").toMutableList()
+                subSources.remove("")
                 val langToUrlMap = mutableMapOf<String, String>()
-                for (subSource in subSources!!) {
+                for (subSource in subSources) {
                     val lang = subSource.substring(subSource.indexOf("[") + 1, subSource.indexOf("]"))
                     val url = subSource.substring(subSource.indexOf("]") + 1).replace("\\","")
                     langToUrlMap[lang] = url
@@ -60,20 +70,21 @@ class SmashyStream{
                 println("Sub: $subSource")
             }
 
-            val vidSources = fileValue?.split(",")?.toMutableList()
-            vidSources?.remove("")
-
-            if (sources[0].contains("dud")) return  Pair(vidSources?.get(0)?.replace("\\",""),subSource)
-
-            val qualityToUrlMap = mutableMapOf<String, String>()
-            for (vidSource in vidSources!!) {
-                val quality =
-                    vidSource.substring(vidSource.indexOf("[") + 1, vidSource.indexOf("]"))
-                val url = vidSource.substring(vidSource.indexOf("]") + 1).replace("\\", "")
-                qualityToUrlMap[quality] = url
-            }
-            println(qualityToUrlMap)
-            return Pair(qualityToUrlMap["auto"],subSource)
+//            val vidSources = fileValue?.split(",")?.toMutableList()
+//            vidSources?.remove("")
+//
+//            if (sources[0].contains("dud")) return  Pair(vidSources?.get(0)?.replace("\\",""),subSource)
+//
+//            val qualityToUrlMap = mutableMapOf<String, String>()
+//            for (vidSource in vidSources!!) {
+//                val quality =
+//                    vidSource.substring(vidSource.indexOf("[") + 1, vidSource.indexOf("]"))
+//                val url = vidSource.substring(vidSource.indexOf("]") + 1).replace("\\", "")
+//                qualityToUrlMap[quality] = url
+//            }
+//            println(qualityToUrlMap)
+//            return Pair(qualityToUrlMap["auto"],subSource)
+            return Pair(streamParsed.sourceUrls[0],subSource)
         }catch (e: Exception){
             e.printStackTrace()
             return Pair(null,null)
